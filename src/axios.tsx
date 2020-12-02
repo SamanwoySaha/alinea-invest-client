4faf6dfad0f6ecc9097cf07eab14768eb4bbbeb4
SamanwoySaha <samanwoy.kuet@gmail.com>
2020-12-02 21:57:51 +0600

    update fetch url

--- src/axios.tsx
+++ src/axios.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://boiling-cliffs-05792.herokuapp.com/",
 });
 
 export default instance;
